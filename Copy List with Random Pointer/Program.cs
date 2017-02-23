6bcbd82c0469735118ed0bbd8442fd89fb4e34ad
Jun <Jun@JUN-PC>
2017-02-22 23:47:51 -0500

    check in

--- Copy List with Random Pointer/Program.cs
+++ Copy List with Random Pointer/Program.cs
@@ -10,6 +10,66 @@ namespace Copy_List_with_Random_Pointer
     {
         static void Main(string[] args)
         {
+            var head = new RandomListNode(1);
+            var node2 = new RandomListNode(2);
+            var node3 = new RandomListNode(3);
+            var node4 = new RandomListNode(4);
+
+            head.next = node2;
+            head.random = node3;
+
+            node2.next = node3;
+            node2.random = head;
+
+            node3.next = node4;
+            node3.random = node4;
+
+            node4.next = null;
+            node4.random = node2;
+
+
+            var headNode = CopyRandomList(head);
+            while (headNode != null)
+            {
+                Console.WriteLine(headNode.label);
+                headNode = headNode.next;
+            }
+
+        }
+
+        public static RandomListNode CopyRandomList(RandomListNode head)
+        {
+            RandomListNode copyNode = head, next;
+
+            while (head != null)
+            {
+                next = head.next;
+                var copy = new RandomListNode(head.label);
+                head.next = copy;
+                copy.next = next;
+
+                head = next;
+            }
+
+            //return copyNode;
+
+            var newCopyNode = copyNode;
+            while (copyNode != null)
+            {
+                if (copyNode.random != null)
+                    copyNode.next.random = copyNode.random.next;
+                copyNode = copyNode.next.next;
+            }
+
+            var finalCopyNode = newCopyNode;
+            while (newCopyNode != null)
+            {
+                var nextNode = newCopyNode.next.next;
+                newCopyNode.next = nextNode;
+                newCopyNode = nextNode;
+            }
+
+            return finalCopyNode;
         }
     }
 }
---
+++ Copy List with Random Pointer/RandomListNode.cs
@@ -0,0 +1,9 @@
+﻿namespace Copy_List_with_Random_Pointer
+{
+    public class RandomListNode
+    {
+        public int label;
+        public RandomListNode next, random;
+        public RandomListNode(int x) { this.label = x; }
+    }
+}
